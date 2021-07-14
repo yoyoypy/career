5c465e941dffd1cedbb56d0ea430d2ea338b3945
yoyoypy <yoppypriyandaru@gmail.com>
2021-07-14 12:04:14 +0700

    add api for job list

---
+++ app/Http/Controllers/API/JobController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Job;
+
+class JobController extends Controller
+{
+    public function all(Request $request)
+    {
+        $id = $request->input('id');
+        $limit = $request->input('limit', 20);
+        $jobtitle = $request->input('jobtitle');
+        $slug = $request->input('slug');
+        $joblocation_id = $request->input('joblocation_id');
+        $jobcategory_id = $request->input('jobcategory_id');
+        $company_id = $request->input('company_id');
+
+        // if($id)
+        // {
+        //     $job = Job::with('Location', 'JobCategory', 'Company')->find($id);
+
+        //     if($id)
+        //         return ResponseFormatter::success($id, 'Data Berhasil Diambil');
+        //     else
+        //         return ResponseFormatter::error(null, 'Data Produk Tidak Ada', 404);
+        // }
+
+
+        if($slug)
+        {
+            $job = Job::with('Location', 'JobCategory', 'Company')
+                    ->where('slug', $slug)->first();
+
+            if($job)
+                return ResponseFormatter::success($job, 'Data Berhasil Diambil');
+            else
+                return ResponseFormatter::error(null, 'Data Produk Tidak Ada', 404);
+        }
+
+        if($joblocation_id)
+        {
+            $job = Job::with('Location', 'JobCategory', 'Company')
+                    ->where('joblocation_id', $joblocation_id)->get();
+
+            if($job)
+                return ResponseFormatter::success($job, 'Data Berhasil Diambil');
+            else
+                return ResponseFormatter::error(null, 'Data Produk Tidak Ada', 404);
+        }
+
+        if($jobcategory_id)
+        {
+            $job = Job::with('Location', 'JobCategory', 'Company')
+                    ->where('jobcategory_id', $jobcategory_id)->get();
+
+            if($job)
+                return ResponseFormatter::success($job, 'Data Berhasil Diambil');
+            else
+                return ResponseFormatter::error(null, 'Data Produk Tidak Ada', 404);
+        }
+
+        if($company_id)
+        {
+            $job = Job::with('Location', 'JobCategory', 'Company')
+                    ->where('company_id', $company_id)->get();
+
+            if($job)
+                return ResponseFormatter::success($job, 'Data Berhasil Diambil');
+            else
+                return ResponseFormatter::error(null, 'Data Produk Tidak Ada', 404);
+        }
+
+
+        $job = Job::with('Location', 'JobCategory', 'Company');
+
+        if($jobtitle)
+            $job->where('jobtitle', 'like', '%'. $jobtitle . '%');
+
+           // return ResponseFormatter::success($produk->paginate(), 'Data List Produk Berhasil Diambil');
+        return $job->paginate($limit);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -13,8 +13,10 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
-Route::post('addskill', 'API\SkillController@addskill');
+// Route::post('addskill', 'API\SkillController@addskill');
+
+Route::get('job', 'API\JobController@all');
